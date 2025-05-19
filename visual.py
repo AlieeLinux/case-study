2f3493ae61f9e4913624daa834acf4f6817399e8
troy600 <bazartroy531@gmail.com>
2025-05-19 11:36:11 +0800

    Statistics is ae

---
+++ visual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot
+
+
+matplotlib.pyplot.bar([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [48, 70, 55, 63, 55, 71, 84, 73, 9, 27, 4, 14])
+matplotlib.pyplot.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [48, 70, 55, 63, 55, 71, 84, 73, 9, 27, 4, 14], color="pink")
+
+
+
+matplotlib.pyplot.show()
+
+
+
